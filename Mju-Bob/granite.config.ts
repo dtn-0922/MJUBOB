8a34a1296158e6987b940bc67ebf9dc6d48c2977
dtn-0922 <163286341+dtn-0922@users.noreply.github.com>
2026-05-13 13:41:27 +0900

    slide_component

--- Mju-Bob/granite.config.ts
+++ Mju-Bob/granite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "Mju-Bob",
   brand: {
-    displayName: "앱 이름", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    displayName: "명지밥", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#FF91D5", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "../images/MJU_BOB_Logo.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
-    host: "localhost",
+    host: "192.168.0.81",
     port: 5173,
     commands: {
-      dev: "vite dev",
+      dev: "vite --host",
       build: "vite build",
     },
   },
